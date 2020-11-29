9e9c3ccf09415d7fe957cfa663bf694940b29083
Hugoles1993 <75224148+Hugoles1993@users.noreply.github.com>
2020-11-29 18:57:46 -0500

    1
    1

---
+++ qgis2web/qgis2web_2020_11_29-18_36_26_685182/data/Centrodeabastecimiento_3.js
@@ -0,0 +1,8 @@
+var json_Centrodeabastecimiento_3 = {
+"type": "FeatureCollection",
+"name": "Centrodeabastecimiento_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Nombre": "Distribuidora" }, "geometry": { "type": "Point", "coordinates": [ -78.654165431952819, -1.676905948985042 ] } }
+]
+}
---
+++ qgis2web/qgis2web_2020_11_29-18_36_26_685182/data/FabricadeCerveza_4.js
@@ -0,0 +1,8 @@
+var json_FabricadeCerveza_4 = {
+"type": "FeatureCollection",
+"name": "FabricadeCerveza_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Nombre": "Fabrica" }, "geometry": { "type": "Point", "coordinates": [ -78.688302538513994, -1.6456364689179 ] } }
+]
+}
